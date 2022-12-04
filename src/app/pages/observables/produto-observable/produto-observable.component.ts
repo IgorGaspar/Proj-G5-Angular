b4d9567a0615007fa98687b25ed5784ffda3d0a9
Bruno E. Silva <bruno.esilva@gmail.com>
2022-12-04 13:15:16 -0300

    Observable Produtos

--- src/app/pages/observables/produto-observable/produto-observable.component.ts
+++ src/app/pages/observables/produto-observable/produto-observable.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from 'src/app/modules/produtos.module';
+import { ProdutosService } from 'src/app/services/produtos.service';
 
 @Component({
   selector: 'app-produto-observable',
@@ -7,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoObservableComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient,
+    private router: Router,
+    private produtosService:ProdutosService
+  ) { }
 
   ngOnInit(): void {
+    this.listarProdutos()
   }
 
-  public produtosCadastrados = 1
+  public produtosCadastrados:Produto[] | undefined = []
+
+  public async listarProdutos(){
+    this.produtosCadastrados = await new ProdutosService(this.http).lista();
+  }
 }
--- src/app/services/produtos.service.ts
+++ src/app/services/produtos.service.ts
@@ -1,12 +1,13 @@
-// import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { HttpClient } from "@angular/common/http";
 import { environment } from "src/environments/environment";
 import { Produto } from "../modules/produtos.module";
+import { Injectable } from '@angular/core';
 
-/* @Injectable({
+@Injectable({
   providedIn: 'root'
-}) */
+})
+
 export class ProdutosService {
 
   constructor(private http:HttpClient) { }
