9284a594dffe80262022a48cdfa7bec8c1e3de80
BeatrizMiotto <miottobiasforever@gmail.com>
2022-12-04 21:21:13 -0300

    delete de pedidos

--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -5,11 +5,14 @@ import {Pedido} from "../modules/pedidos.module";
 
 
 export class PedidosService {
-
+  
   constructor(private http:HttpClient) { }
 
   public async lista(){
     let pedidos:Pedido[] | undefined = await firstValueFrom(this.http.get<Pedido[]>(`${environment.api}/pedidos`))
     return  pedidos;
   }
+  public excluirPedido(id:Number) {
+    firstValueFrom(this.http.delete(`${environment.api}/pedidos/${id}`))
+}
 }
